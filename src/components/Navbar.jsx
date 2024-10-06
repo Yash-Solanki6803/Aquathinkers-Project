e4a8125d753432479de39305e4bf7c15bc1d9303
sonidhruv26 <sonidhruv.2603@gmail.com>
2024-10-06 10:35:59 +0530

    Add Navbar styling and update Home page layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,16 @@
 import { Link } from "react-router-dom";
+// import "./Navbar.css";
 
 function Navbar() {
   return (
-    <nav>
-      <ul>
+    <nav className="w-4/6 bg-slate-400 fixed top-4 left-4 p-2 rounded-xl backdrop-blur-md shadow">
+      <ul className="flex flex-row justify-evenly content-center">
+        <h1>
+          
+        </h1>
+
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/">Aquathinkers</Link>
         </li>
         <li>
           <Link to="/about">About</Link>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,13 @@
 import { useStore } from "../store";
-import { data } from "../data/homepage";
 
 function Home() {
   const synthesisType = useStore((state) => state.synthesisType);
   const toggleSynthesis = useStore((state) => state.toggleSynthesis);
   return (
     <>
-      
-      <br />
-      <p>{synthesisType?"Chemo":"Photo"}</p>
-      <button onClick={toggleSynthesis}>Toggle Synthesis</button>
+      <div className="bg-slate-700">
+        <h1 className="p-44">Home</h1>
+      </div>
     </>
   );
 }
