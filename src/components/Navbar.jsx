208a82d6713c4a14cd155006dab0c04ff7d8c82b
sonidhruv26 <sonidhruv.2603@gmail.com>
2024-10-06 11:04:28 +0530

    Update Navbar styling and Home page background image

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,21 @@
 import { Link } from "react-router-dom";
-// import "./Navbar.css";
+import "./Navbar.css";
 
 function Navbar() {
   return (
-    <nav className="w-4/6 bg-slate-400 fixed top-4 left-4 p-2 rounded-xl backdrop-blur-md shadow">
-      <ul className="flex flex-row justify-evenly content-center">
-        <h1>
-          
-        </h1>
-
-        <li>
-          <Link to="/">Aquathinkers</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/team">Team</Link>
-        </li>
+    <nav className="blur-navbar w-3/6 fixed top-4 left-4 py-2 px-8 rounded-xl flex justify-between content-center">
+      <ul className="w-full flex justify-between content-center">
+        <div>
+            <Link to="/" className="text-xl">Aquathinkers</Link>
+        </div>
+        <div className="flex flex-row justify-evenly content-center gap-20 text-xl">
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/team">Team</Link>
+          </li>
+        </div>
       </ul>
     </nav>
   );
