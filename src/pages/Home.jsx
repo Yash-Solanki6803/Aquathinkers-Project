49aef343193aa5ce6c925d46b8f1d2397d385add
Yash Solanki <thisisyashs@gmail.com>
2024-10-06 16:17:36 +0530

    Introduction Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,33 @@
 import { useStore } from "../store";
+// import { data } from "../data/homepage";
+import { useEffect } from "react";
 
 function Home() {
   const synthesisType = useStore((state) => state.synthesisType);
   const toggleSynthesis = useStore((state) => state.toggleSynthesis);
+
+  useEffect(() => {
+    // Add or remove the 'dark' class from the body based on the synthesisType
+    if (synthesisType) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [synthesisType]);
   return (
     <>
-      <div className="bg-slate-700" style={{"background": "url('https://plus.unsplash.com/premium_photo-1666900440561-94dcb6865554?q=80&w=1854&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D') no-repeat center center fixed", "backgroundSize": "cover"}}>
-        <h1 className="p-44">Home</h1>
+      <div
+        className="bg-slate-700"
+        style={{
+          background:
+            "url('https://plus.unsplash.com/premium_photo-1666900440561-94dcb6865554?q=80&w=1854&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D') no-repeat center center fixed",
+          backgroundSize: "cover",
+        }}
+      >
+        <h1 className="p-44 dark:text-red-600">
+          {synthesisType ? "Chemosynthesis" : "Photosynthesis"}
+        </h1>
+        <button onClick={toggleSynthesis}>Toggle VIeww</button>
       </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "sky-gradient":
+          "radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(118,201,255,1) 11%, rgba(0,212,255,1) 100%)",
+        "water-gradient-1":
+          "linear-gradient(180deg,rgba(255,255,255,0.5) 0%, rgba(115,168,240,1) 2%, rgba(57,133,186,1) 46%, rgba(39,109,154,1) 66%, rgba(14,70,94,1) 100%);",
+        "water-gradient-2":
+          "linear-gradient(180deg, rgba(14,70,94,1) 0%, rgba(13,53,82,1) 46%, rgba(5,35,55,1) 70%, rgba(2,24,33,1) 100%);",
+        "water-gradient-3":
+          "linear-gradient(180deg, rgba(2,24,33,1) 0%, rgba(0,0,0,1) 100%);",
+      },
+      dropShadow: {
+        dark: "0px 4px 100px rgba(0, 0, 0, 1)", // Customize the values as needed
+        light: "2px 4px 200px rgb(89, 89, 89)", // Customize the values as needed
+      },
+    },
   },
+  darkMode: "class",
   plugins: [],
 };
